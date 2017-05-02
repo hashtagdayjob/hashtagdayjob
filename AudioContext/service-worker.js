e229826a385870ab5cbad4702b6b8ac816b8e8ba
Nigel Wade <nigel@dialexy.com>
2017-05-02 12:43:34 +0100

    Update cache list

--- AudioContext/service-worker.js
+++ AudioContext/service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'dialexy-v4';
 const urlsToCache = [
-    'index.html',
-    'css/style.css',
-    'audio/In The Silo Revised.wav',
-    'js/audioContext.js',
-    'js/require.js',
-    'js/simplethiser.js',
+    'AudioContext/index.html',
+    'AudioContext/css/style.css',
+    'AudioContext/audio/In The Silo Revised.wav',
+    'AudioContext/js/audioContext.js',
+    'AudioContext/js/require.js',
+    'AudioContext/js/simplethiser.js',
 ];
 self.addEventListener('install', function(event) {
     console.log(event);

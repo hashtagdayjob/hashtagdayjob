d70c586b3055ddfcd6f8dbb98ee951bb18ea9fd0
nigeljohnwade <nigeljohnwade@gmail.com>
2017-05-14 21:09:20 +0100

    Adds images and your yube video resizer

--- js/youTubeBackground.js
+++ js/youTubeBackground.js
@@ -1,26 +1,31 @@
 const youTubeBackground = {
     init: function(){
-        this.element = document.querySelector('.youTubeBackground');
-        this.viewportWidth = window.innerWidth;
-        this.viewportHeight = window.innerHeight;
+        this.element = document.querySelector('.app-youTubeBackground');
+        this.elementWrapper = document.querySelector('.app-youTubeBackgroundWrapper');
         this.youTubeBackgroundWidth = this.element.clientWidth;
         this.youTubeBackgroundHeight = this.element.clientHeight;
-        this.heightScale = this.viewportHeight/this.youTubeBackgroundHeight;
-        this.widthScale = this.viewportWidth/this.youTubeBackgroundWidth;
+        this.heightScale = this.viewportHeight()/this.youTubeBackgroundHeight;
+        this.widthScale = this.viewportWidth()/this.youTubeBackgroundWidth;
         this._resizeHandler();
-        console.log(this);
+        window.addEventListener('resize', this._resizeHandler);
+    },
+    viewportWidth: function(){
+        return window.innerWidth;
+    },
+    viewportHeight: function(){
+        return window.innerHeight;
     },
     _setHeight: function(){
         switch (this.orientation) {
             case 'portrait':
                 this.element.height = this.youTubeBackgroundHeight * this.heightScale;
                 this.element.width = this.youTubeBackgroundWidth * this.heightScale;
-                this.element.style.left = (this.viewportWidth - this.element.width)/2 + 'px';
+                this.element.style.marginLeft = (this.viewportWidth() - this.element.width)/2 + 'px';
                 break;
             case 'landscape':
                 this.element.height = this.youTubeBackgroundHeight * this.widthScale;
                 this.element.width = this.youTubeBackgroundWidth * this.widthScale;
-                this.element.style.top = (this.viewportHeight - this.element.height)/2 + 'px';
+                this.element.style.marginTop = (this.viewportHeight() - this.element.height)/2 + 'px';
                 break;
         }
     },
@@ -28,8 +33,8 @@ const youTubeBackground = {
         return this.heightScale - this.widthScale > 0 ? 'portrait' : 'landscape';
     },
     _resizeHandler: function(){
-        this.orientation = this._detectOrientation();
-        this._setHeight();
+        youTubeBackground.orientation = youTubeBackground._detectOrientation();
+        youTubeBackground._setHeight();
     }
 }
 youTubeBackground.init();

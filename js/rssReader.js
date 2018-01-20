630380688344c9cb3bc846e9ecfa3cf24ebb1132
Nigel Wade <nigeljohnwade@gmail.com>
2018-01-20 12:06:24 +0000

    Parse titles from RSS feed

--- js/rssReader.js
+++ js/rssReader.js
@@ -4,14 +4,16 @@ function rssReader(url){
             done: function (xhr) {
                 const parser = new DOMParser();
                 const xmlDoc = parser.parseFromString(xhr.responseText, 'text/xml');
-                let items = Array.from(xmlDoc.getElementsByTagName('guid'));
-                items = items.map((item)=>{
-                    return item.textContent.substring(item.textContent.lastIndexOf('/') + 1);
-                });
-                items = items.map((element) => {
-                    return `<p data-height="791" data-theme-id="0" data-slug-hash="${element}" data-default-tab="result" data-user="nigeljohnwade"
+                let items = {
+                    guids: Array.from(xmlDoc.getElementsByTagName('guid')),
+                    titles: Array.from(xmlDoc.getElementsByTagName('title'))
+                };
+                items.titles.shift();
+                items = items.guids.map((element, index) => {
+                    let guid = element.textContent.substring(element.textContent.lastIndexOf('/') + 1);
+                    return `<p data-height="791" data-theme-id="0" data-slug-hash="${guid}" data-default-tab="result" data-user="nigeljohnwade"
                             data-embed-version="2" data-pen-title="Pan/Zoom Canvas 4" data-preview="true" class="codepen">See the Pen <a
-                            href="https://codepen.io/nigeljohnwade/pen/${element}/">Pan/Zoom Canvas 4</a> by Nigel Wade (<a
+                            href="https://codepen.io/nigeljohnwade/pen/${guid}/">${items.titles[index].textContent}</a> by Nigel Wade (<a
                             href="https://codepen.io/nigeljohnwade">@nigeljohnwade</a>) on <a href="https://codepen.io">CodePen</a>.</p>`
                 });
                 let descriptions = Array.from(xmlDoc.getElementsByTagName('description')).map((element, index, array)=>{
@@ -21,7 +23,7 @@ function rssReader(url){
                 let script = document.createElement('script');
                 script.src = 'https://production-assets.codepen.io/assets/embed/ei.js';
                 document.head.appendChild(script);
-                console.log(descriptions);
+                console.log(items);
             }
         }
         , 'GET'

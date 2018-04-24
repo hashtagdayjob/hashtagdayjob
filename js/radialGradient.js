e8e4c50747ad0f34ee37ea2281a3ab6055bdcb5c
Nigel Wade <nigel.wade@zonal.co.uk>
2018-04-24 16:17:09 +0100

    Initial commit of working paintlet

--- js/radialGradient.js
+++ js/radialGradient.js
@@ -1,6 +1,10 @@
 class RadialGradientPainter {
-    paint(ctx, size) {
-        gradient = ctx.createRadialGradient(
+    static get inputProperties() {
+        return ['border-top-color', 'border-bottom-color'];
+    }
+
+    paint(ctx, size, props) {
+        const gradient = ctx.createRadialGradient(
             size.width / 2,
             size.height / 2,
             0,
@@ -8,33 +12,17 @@ class RadialGradientPainter {
             size.height / 2,
             size.width / 2,
         );
-        stops = [
-            [0, 'rgba(100, 100, 150, 0.5)'],
-            [0.4, 'rgba(100, 250, 100, 1)'],
-            [1, 'rgba(150, 100, 100, 0.5)']
-        ],
+        const stops = [
+            [0, props.get('border-top-color')],
+            [0.4, props.get('border-bottom-color')],
+            [1, props.get('border-top-color')]
+        ];
         stops.map((element) => {
             gradient.addColorStop(element[0], element[1]);
         });
         ctx.rect(0, 0, size.height, size.width);
-        ctx.fillStyle = radialGradient(
-            ctx
-        );
+        ctx.fillStyle = gradient;
         ctx.fill();
-        // ctx.lineWidth = 2;
-        // ctx.strokeStyle = '#666';
-
-        // // draw line from top left to bottom right
-        // ctx.beginPath();
-        // ctx.moveTo(0, 0);
-        // ctx.lineTo(size.width, size.height);
-        // ctx.stroke();
-
-        // // draw line from top right to bottom left
-        // ctx.beginPath();
-        // ctx.moveTo(size.width, 0);
-        // ctx.lineTo(0, size.height);
-        // ctx.stroke();
     }
 }
 

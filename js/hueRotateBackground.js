ea08488434d2277fc501581c2afa4dd88022b12c
nigeljohnwade <nigeljohnwade@gmail.com>
2019-06-02 22:56:05 +0100

    Houdini article

--- js/hueRotateBackground.js
+++ js/hueRotateBackground.js
@@ -4,13 +4,12 @@ class HueRotateBackground{
     
     paint(ctx, geom, properties) {
         let tick = parseFloat(properties.get('--animation-tick').toString());
-        console.log(tick);
         let hue = Math.sin(tick/10000) + 1;
-        ctx.fillStyle = 'hsl(' + parseInt(hue * 180)  + ', 100%, 50%)';
+        ctx.fillStyle = `hsl(${parseInt(hue * 180)}, 100%, 50%)`;
         ctx.fillRect(0, 0, geom.width, geom.height);
         ctx.fill();
     }
     
-});
+}
 
 registerPaint('hue-rotate-background', HueRotateBackground);
--- js/radialGradient.js
+++ js/radialGradient.js
@@ -1,9 +1,9 @@
-class RadialGradientPainter {
+class RadialGradient {
     
     static get inputProperties() {
         return [
-            'border-top-color',
-            'border-bottom-color',
+            '--gradient-start-color',
+            '--gradient-end-color',
             '--animation-tick',
             '--gradient-divisor'
         ];
@@ -12,18 +12,17 @@ class RadialGradientPainter {
     paint(ctx, size, props) {
         let tick = parseFloat(props.get('--animation-tick').toString());
         let divisor = parseFloat(props.get('--gradient-divisor').toString());
-        console.log(Math.sin(tick/1000));
         const gradient = ctx.createRadialGradient(
             (Math.sin(tick/1000) * size.width / 2) + size.width / 2,
-            (Math.cos(tick/1000) * size.height / 2) +size.height / 2,
+            (Math.cos(tick/500) * size.height / 2) +size.height / 2,
             0,
             size.width / divisor,
             size.height / divisor,
             size.width,
         );
         const stops = [
-            [0, props.get('border-top-color')],
-            [1, props.get('border-bottom-color')]
+            [0, props.get('--gradient-start-color')],
+            [1, props.get('--gradient-end-color')],
         ];
         stops.map((element) => {
             gradient.addColorStop(element[0], element[1]);
@@ -35,4 +34,4 @@ class RadialGradientPainter {
     
 }
 
-registerPaint('radial-gradient', RadialGradientPainter);
+registerPaint('radial-gradient', RadialGradient);
